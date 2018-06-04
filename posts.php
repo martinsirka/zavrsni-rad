9c091c6d0c56644684fb74273d3900b24a68fdde
Martin Sirka <martinsirka@Martins-MacBook-Pro.local>
2018-06-04 22:02:02 +0200

    connetion to db

--- posts.php
+++ posts.php
@@ -1,5 +1,27 @@
-<?php include 'partials/header.php'; ?>
+<?php 
+include 'partials/header.php';
 
-<?php include 'partials/sidebar.php'; ?>
+include 'conection_to_db.php';
 
-<?php include 'partials/footer.php'; ?>
+$sql = "SELECT id, title, created_at, body, author FROM posts ORDER BY created_at DESC LIMIT 3";
+    $statement = $connection->prepare($sql);
+
+    // izvrsavamo upit
+    $statement->execute();
+
+    // zelimo da se rezultat vrati kao asocijativni niz.
+    // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+    // punimo promenjivu sa rezultatom upita
+    $posts = $statement->fetchAll();
+
+    // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+        echo '<pre>';
+        var_dump($posts);
+        echo '</pre>';
+
+
+include 'partials/sidebar.php'; 
+include 'partials/footer.php'; 
+?>
